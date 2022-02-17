341678930447a0c5c27456c0ec8558b116b50ef2
Jc <Jc@email.com>
2022-02-17 15:07:11 +0000

    completed challenge ez

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,11 @@
 // TODO (Ex.0): document and write tests for this function
-
+/**
+ * 
+ * @param {number} n - user input number
+ * @returns {number} - negative value of input
+ */
 function asNegative(n) {
-  // ...
+  return n<0? n: -n;
 }
 
-
+console.log("asNegative(10)", asNegative(10), "should equal -10")
